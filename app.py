6757b12b37b6f75e65dbd6a56a18afe3e801d122
bluedaze <35676690+bluedaze@users.noreply.github.com>
2021-12-08 15:30:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,168 +3,20 @@ import requests
 import requests.auth
 from flask import Flask, render_template
 from credentials import CLIENT_ID, SECRET_TOKEN
-import youtube_dl
-
+import yt_dlp
+from parser import Parser
+import sqlite3
+import pickle
+import uuid
 app = Flask(__name__)
-
-class Thread:
-    def __init__(self):
-        self.post_id = ""
-        self.tag = ""
-        self.url = ""
-        self.image = ""
-        self.name = ""
-        self.tokens = []
-        self.comments = []
-        self.feat = []
-        self.text = ""
-
-class Tokenizer:
-    def __init__(self, child):
-        self.thread = Thread()
-        self.constructToken(child)
-        self.pos = 0
-        self.char = ""
-
-    def constructToken(self, child):
-        self.thread.text = child["data"]["title"]
-        self.thread.url = child["data"]["url"]
-        self.tokens = self.thread.tokens
-        self.text = self.thread.text
-
-    def normalizeCharacters(self):
-        # There are some characters that look weird, or are harder to parse. So we discard those.
-        if "&amp;" in self.text:
-            self.text = self.text.replace("&amp;", "&")
-        if "–" in self.text:
-            self.text = self.text.replace("–", "-")
-
-    def getID(self):
-        result = ""
-        while self.char.isalnum() and self.pos < len(self.text):
-            result += self.char
-            self.nextChar()
-        self.tokens.append(result)
-
-    def peek(self):
-        if self.pos < len(self.text):
-            try:
-                nextChar = self.text[self.pos + 1]
-                return nextChar
-            except:
-                return None
-
-    def addToken(self, result):
-        self.tokens.append(result)
-        self.nextChar()
-
-    def nextChar(self):
-        self.pos += 1
-        if self.pos < len(self.text):
-            self.char = self.text[self.pos]
-
-    def createToken(self):
-        if self.char.isalnum():
-            self.getID()
-        elif self.char.isspace():
-            self.addToken(self.char)
-        elif self.char:
-            self.addToken(self.char)
-        else:
-            self.nextChar()
-
-    def parseText(self):
-        self.normalizeCharacters()
-        self.char = self.text[self.pos]
-        while self.pos < len(self.text):
-            self.createToken()
-        return self.thread
-
-class parser:
-    def __init__(self, child):
-        self.thread = Tokenizer(child).parseText()
-        self.tokens = self.thread.tokens
-        self.pos = 0
-        self.currentToken = self.thread.tokens[self.pos]
-        self.parse()
-
-    def parseParens(self):
-        comment = ""
-        while self.currentToken != ")":
-            comment += self.currentToken
-            self.advance()
-        self.thread.comments.append(comment)
-        self.advance()
-
-    def advance(self):
-        self.pos += 1
-        if self.pos < len(self.tokens):
-            self.currentToken = self.tokens[self.pos]
-
-    def nextToken(self):
-        nextToken = ""
-        if self.pos < len(self.tokens):
-            nextToken = self.tokens[self.pos + 1]
-        return nextToken
-
-    def parseSquarebrackets(self):
-        tag = ""
-        keywords = ["fresh", "video", "album", "EP"]
-        while self.currentToken != "]":
-            if self.currentToken.lower() in keywords or self.currentToken.isspace():
-                tag += self.currentToken.upper()
-                self.advance()
-            else:
-                self.advance()
-        self.thread.tag = tag
-        self.advance()
-
-    def getName(self):
-        stopChars = ["(", "["]
-        while self.pos < len(self.tokens):
-            if self.currentToken in stopChars:
-                self.thread.name = self.thread.name.rstrip()
-                break
-            elif self.currentToken:
-                self.thread.name += self.currentToken
-                self.advance()
-
-    def containAlphaNum(self):
-        if any(char.isalnum() for char in self.currentToken):
-            return True
-        else:
-            return False
-
-    def parseTokens(self):
-        if self.currentToken == "[":
-            self.parseSquarebrackets()
-        elif self.currentToken == "(":
-            self.parseParens()
-        elif self.currentToken.isalnum():
-            self.getName()
-        elif self.currentToken.isspace():
-            self.advance()
-        elif self.containAlphaNum():
-            self.getName()
-        else:
-            self.getName()
-
-    def parse(self):
-        while self.pos < len(self.tokens):
-            self.parseTokens()
-        return self.thread
 
 class redditRequest:
     def __init__(self):
-        self.data = {}
-        self.tracks = []
-        self.videos = []
-        self.albums = []
-        self.index = ""
-        self.token = ""
+        self.data = []
+        self.access_token = ""
         self.headers = {"User-Agent": "hhhcli"}
         self.params = {"limit": "100"}
-        self.main()
+        self.makeRequests()
 
     def getToken(self):
         client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_TOKEN)
@@ -176,8 +28,8 @@ class redditRequest:
             headers=self.headers,
         )
         rj = response.json()
-        self.token = rj["access_token"]
-        self.headers = {"Authorization": f"bearer {self.token}", "User-Agent": "hhhcli"}
+        self.access_token = rj["access_token"]
+        self.headers = {"Authorization": f"bearer {self.access_token}", "User-Agent": "hhhcli"}
 
     def requestPosts(self):
         self.getToken()
@@ -190,67 +42,177 @@ class redditRequest:
         response = res.json()
         return response
 
-    def pingYoutube(self, thread):
-        isVideo = True
-        ydl = youtube_dl.YoutubeDL({"simulate": True, "quiet": True, "forcethumbnail": True})
-        with ydl:
-            try:
-                result = ydl.extract_info(thread.url, download=False, ie_key="Youtube")
-                thread.url = result["webpage_url"]
-                thread.image = result["thumbnails"][-1]["url"]
-            except Exception as e:
-                isVideo = False
-        return isVideo
+    def parseResponse(self, response):
+        for item in response:
+            if item["data"]["ups"] > 10:
+                thread = Parser(item).parse()
+                self.data.append(thread)
 
-    def configureVideo(self, thread):
-        isVideo = self.pingYoutube(thread)
-        if isVideo:
-            self.videos.append(thread)
-
-    def parseRequest(self, response):
-        children = response["data"]["children"]
-        self.index = children[-1]["data"]["name"]
-        for child in children:
-            if child["data"]["ups"] >= 10:
-                thread = parser(child).parse()
-                if thread.tag == "FRESH":
-                    self.tracks.append(thread)
-                elif thread.tag == "FRESH VIDEO":
-                    self.configureVideo(thread)
-                elif thread.tag == "FRESH ALBUM":
-                    self.albums.append(thread)
-
-    def main(self):
+    def makeRequests(self):
         # We call three times because Reddit search api only returns data the first three times
         # When I looked up why this happens it seems that the number of results has a cap.
         # Supposedly this is around 1,000 returned results.
         # I suspect this isn't true. I think instead you are only allowed to search up to 1,000
         # posts back - starting with the most recent post.
         for i in range(3):
-            response = self.requestPosts()
-            self.parseRequest(response)
-            # We update the parameters after performing a search so that we can start a search after the index.
+            request = self.requestPosts()
+            response = request["data"]["children"]
+            self.parseResponse(response)
+            # We update the parameters after performing a search
+            # so that we can start a search after the index.
             # The index in this case is the "name" of the last post returned.
-            self.params = {"limit": "100", "after": self.index}
+            index = response[-1]["data"]["name"]
+            self.params = {"limit": "100", "after": index}
 
+class parseResponse:
+    def __init__(self, data):
+        self.data = data
+        self.tracks = []
+        self.videos = []
+        self.albums = []
+        self.parseData()
 
-@app.route("/")
-@app.route("/index")
+    def parseData(self):
+        for thread in self.data:
+            if thread["tag"] == "FRESH":
+                self.configureTrack(thread)
+            elif thread["tag"] == "FRESH VIDEO":
+                self.configureVideo(thread)
+            elif thread["tag"] == "FRESH ALBUM" or thread["tag"] == "FRESH EP":
+                self.albums.append(thread)
+
+    def pingYoutube(self, thread):
+        isVideo = True
+        ydl = yt_dlp.YoutubeDL({"simulate": True, "quiet": True, "preference": 0})
+        with ydl:
+            try:
+                result = ydl.extract_info(thread["url"], download=False, process=False, ie_key="Youtube")
+                thread["url"] = result["webpage_url"]
+                thread["ytid"] = result["id"]
+                thread['image'] = result["thumbnail"]
+            except Exception:
+                isVideo = False
+        return isVideo
+
+    def configureTrack(self, thread):
+        # This function is probably unnecessary
+        # But I didn't like how there were several
+        # url formats for YouTube links.
+        isYoutube = thread["url"].find('youtu')
+        if isYoutube >= 0:
+            self.pingYoutube(thread)
+        self.tracks.append(thread)
+
+    def configureVideo(self, thread):
+        isVideo = self.pingYoutube(thread)
+        if isVideo:
+            self.videos.append(thread)
+
+        thread = {"post_id": "",
+                  "tag": "",
+                  "url": "",
+                  "image": "",
+                  "name": "",
+                  "tokens": [],
+                  "comments": [],
+                  "feat": [],
+                  "text": ""}
+
+class Db:
+    def __init__(self):
+        pass
+
+    def create_db(self):
+        conn = sqlite3.connect("refresher.db")
+        c = conn.cursor()
+        c.execute(
+            "CREATE TABLE IF NOT EXISTS refresher (post_id TEXT, tag TEXT, url TEXT, image TEXT, name TEXT, tokens TEXT, comments TEXT, feat TEXT, text TEXT)"
+        )
+        c.close()
+        conn.close()
+
 def main():
-    print("Creating new request")
-    newRequest = redditRequest()
-    print("Request retrieved")
+    request = redditRequest()
+    response = parseResponse(request.data)
     if __name__ == "__main__":
         pass
     else:
         return render_template(
             "base.html",
-            videos=newRequest.videos,
-            tracks=newRequest.tracks,
-            albums=newRequest.albums,
+            videos=response.videos,
+            tracks=response.tracks,
+            albums=response.albums,
             zip=zip,
         )
 
+
+# Created a json api.
+# Got bored, and didn't want to write a database.
+@app.route("/data.json")
+def dataJson():
+    request = redditRequest()
+    response = parseResponse(request.data)
+    response ={"albums": response.albums, "tracks": response.tracks}
+    if __name__ == "__main__":
+        pass
+    else:
+        return response
+
+
+# The pickle stuff is pretty useful for rapid prototyping
+# if you don't want to go through the effort of setting up
+# a database.
+def savePickle(response):
+    file_to_store = open("stored_object.pickle", "wb")
+    pickle.dump(response, file_to_store)
+    file_to_store.close()
+
+def loadPickle():
+    file_to_read = open("stored_object.pickle", "rb")
+    response = pickle.load(file_to_read)
+    file_to_read.close()
+    return response
+
+def start_app():
+    request = redditRequest()
+    response = parseResponse(request.data)
+    return response
+
+def pickelize(pickle_status):
+    response = ""
+    if pickle_status == "load":
+        response = loadPickle()
+    elif pickle_status == "save":
+        response = start_app()
+        savePickle(response)
+    elif pickle_status == None:
+        response = start_app()
+    return response
+
+@app.route("/")
+@app.route("/index")
+@app.route("/table")
+def render_table():
+    response = pickelize("load")
+    if __name__ == "__main__":
+        pass
+    else:
+        return render_template(
+            "table.html",
+            videos=response.videos,
+            tracks=response.tracks,
+            albums=response.albums,
+            zip=zip,
+            uuid=uuid
+        )
+
+# This route is useful for prototyping some js
+# or w/e real quick.
+@app.route("/test")
+def testFetch():
+    response = pickelize("save")
+    return render_template("test.html",
+                           videos=response.videos)
+
 if __name__ == "__main__":
     main()
-    thread = "https://www.youtube.com/watch?v=yGYuDtF5AnE"
---
+++ parser.py
@@ -0,0 +1,150 @@
+class Tokenizer:
+    def __init__(self, child):
+        self.thread = self.spawnThread()
+        self.constructToken(child)
+        self.pos = 0
+        self.char = ""
+
+    def spawnThread(self):
+        thread = {"post_id": "",
+                  "reddit_id": "",
+                  "tag": "",
+                  "url": "",
+                  "image": "",
+                  "name": "",
+                  "tokens": [],
+                  "comments": [],
+                  "feat": [],
+                  "text": "",
+                  "ytid": "",
+                  "ups": ""}
+        return thread
+
+    def constructToken(self, child):
+        self.thread["text"] = child["data"]["title"]
+        self.thread["url"] = child["data"]["url"]
+        self.thread["reddit_id"] = child["data"]["id"]
+        self.thread["ups"] = child["data"]["ups"]
+
+    def sanitize(self):
+        # There are some characters that look weird,
+        # or are harder to parse. So we replace those.
+        if "&amp;" in self.thread["text"]:
+            self.thread["text"] = self.thread["text"].replace("&amp;", "&")
+        if "–" in self.thread["text"]:
+            self.thread["text"] = self.thread["text"].replace("–", "-")
+
+    def getID(self):
+        result = ""
+        while self.char.isalnum() and self.pos < len(self.thread["text"]):
+            result += self.char
+            self.nextChar()
+        self.thread["tokens"].append(result)
+
+    def peek(self):
+        if self.pos < len(self.thread["text"]):
+            try:
+                nextChar = self.thread["text"][self.pos + 1]
+                return nextChar
+            except:
+                return None
+
+    def addToken(self, result):
+        self.thread["tokens"].append(result)
+        self.nextChar()
+
+    def nextChar(self):
+        self.pos += 1
+        if self.pos < len(self.thread["text"]):
+            self.char = self.thread["text"][self.pos]
+
+    def createToken(self):
+        if self.char.isalnum():
+            self.getID()
+        elif self.char.isspace():
+            self.addToken(self.char)
+        elif self.char:
+            self.addToken(self.char)
+        else:
+            self.nextChar()
+
+    def parseText(self):
+        self.sanitize()
+        self.char = self.thread["text"][self.pos]
+        while self.pos < len(self.thread["text"]):
+            self.createToken()
+        return self.thread
+
+class Parser:
+    def __init__(self, child):
+        self.thread = Tokenizer(child).parseText()
+        self.tokens = self.thread["tokens"]
+        self.pos = 0
+        self.currentToken = self.thread["tokens"][self.pos]
+        self.parse()
+
+    def parseParens(self):
+        comment = ""
+        while self.currentToken != ")":
+            comment += self.currentToken
+            self.advance()
+        self.thread["comments"].append(comment)
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.tokens):
+            self.currentToken = self.tokens[self.pos]
+
+    def nextToken(self):
+        nextToken = ""
+        if self.pos < len(self.tokens):
+            nextToken = self.tokens[self.pos + 1]
+        return nextToken
+
+    def parseSquarebrackets(self):
+        tag = ""
+        keywords = ["fresh", "video", "album", "EP"]
+        while self.currentToken != "]":
+            if self.currentToken.lower() in keywords or self.currentToken.isspace():
+                tag += self.currentToken.upper()
+                self.advance()
+            else:
+                self.advance()
+        self.thread["tag"] = tag
+        self.advance()
+
+    def getName(self):
+        stopChars = ["(", "["]
+        while self.pos < len(self.tokens):
+            if self.currentToken in stopChars:
+                self.thread["name"] = self.thread["name"].rstrip()
+                break
+            elif self.currentToken:
+                self.thread["name"] += self.currentToken
+                self.advance()
+
+    def containAlphaNum(self):
+        if any(char.isalnum() for char in self.currentToken):
+            return True
+        else:
+            return False
+
+    def parseTokens(self):
+        if self.currentToken == "[":
+            self.parseSquarebrackets()
+        elif self.currentToken == "(":
+            self.parseParens()
+        elif self.currentToken.isalnum():
+            self.getName()
+        elif self.currentToken.isspace():
+            self.advance()
+        elif self.containAlphaNum():
+            self.getName()
+        else:
+            self.getName()
+
+    def parse(self):
+        while self.pos < len(self.tokens):
+            self.parseTokens()
+        return self.thread
